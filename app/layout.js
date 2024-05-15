60eb48450e13dd389b5a7bf66d1453b8194bd51e
berkay-yuksel <98.berkay.yuksel@gmail.com>
2024-05-15 15:17:23 +0300

    mostly size fix

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 /* eslint-disable @next/next/no-head-element */
 import localFont from "@next/font/local";
-
+import "../styles/globals.css";
 const myFont = localFont({ src: "../fonts/FoundersGrotesk-Regular.otf" });
 
 export default function RootLayout({ children }) {
