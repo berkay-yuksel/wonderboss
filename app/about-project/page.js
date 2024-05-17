21641a06fe5f3d570c63991a0d4588e7b6477999
berkay-yuksel <98.berkay.yuksel@gmail.com>
2024-05-18 01:29:40 +0300

    mostly size fix

--- app/about-project/page.js
+++ app/about-project/page.js
@@ -1,20 +1,85 @@
 import Navbar from "../../components/Navbar";
 import mintInfo from "../../styles/Mintinfo.module.css";
+import aboutProject from "../../styles/Aboutproject.module.css";
 import Image from "next/image";
 import Link from "next/link";
 
 const AboutProject = () => {
   return (
     <div className={mintInfo.bigger}>
-      <div className={mintInfo.container}>
+      <div>
         <Link href="/">BACK TO HOME PAGE</Link>
-        <h1>ABOUT FOMO SAPIENS</h1>
+        <h1>LEARN MORE</h1>
         <div className="team_container">
-          <h5>CORE TEAM</h5>
-          teammm
+          <h2>CORE TEAM</h2>
+          <div className={aboutProject.team_container}>
+            <div>
+              <Image
+                className="{mintInfo.banner}"
+                src="/lunaris.webp"
+                alt="Picture of the author"
+                width="300"
+                height="300"
+                blurDataURL="/mintinfo.webp"
+                placeholder="/mintinfo.webp"
+              />
+              <span>
+                {" "}
+                <Link href="/">
+                  <h3>lunaris</h3>
+                  <h5>co-founder & project manager</h5>
+                </Link>
+              </span>
+            </div>
+            <div>
+              <Image
+                className="{mintInfo.banner}"
+                src="/nac.png"
+                alt="Picture of the author"
+                width="300"
+                height="300"
+                blurDataURL="/mintinfo.webp"
+                placeholder="/mintinfo.webp"
+              />
+              <Link href="/">
+                <h3>nac</h3>
+                <h5>co-founder & community manager</h5>
+              </Link>
+            </div>
+            <div>
+              <Image
+                className="{mintInfo.banner}"
+                src="/tmac.jpg"
+                alt="Picture of the author"
+                width="300"
+                height="300"
+                blurDataURL="/mintinfo.webp"
+                placeholder="/mintinfo.webp"
+              />
+              <Link href="/">
+                <h3>tmac</h3>
+                <h5>artist</h5>
+              </Link>
+            </div>
+            <div>
+              <Image
+                className="{mintInfo.banner}"
+                src="/cuneyt.png"
+                alt="Picture of the author"
+                width="300"
+                height="300"
+                blurDataURL="/mintinfo.webp"
+                placeholder="/mintinfo.webp"
+              />
+              <Link href="/">
+                <h3>cuneyt senyavas</h3>
+                <h5>technical lead</h5>
+              </Link>
+            </div>
+          </div>
         </div>
-        <div>
-          <h5>What is Fomo Sapiens?</h5>
+        <div className={aboutProject.text_container}>
+          <h2>What is Fomo Sapiens?</h2>
           <p>
             Fomo Sapiens is a unique PFP NFT collection comprising 7777 distinct
             pieces. At its core, the project is dedicated to fostering a vibrant
@@ -35,22 +100,29 @@ const AboutProject = () => {
             intellectual property(IP).
           </p>
           <br />
-          <Image
-            className="{mintInfo.banner}"
-            src="/mintinfo.png"
-            alt="Picture of the author"
-            width="560"
-            height="290"
-            blurDataURL="/mintinfo.webp"
-            placeholder="/mintinfo.webp"
-          />
-          <h5>Pillars of Fomo Sapiens:</h5>
+          <div className={aboutProject.first_image_container}>
+            <Image
+              src="/story2.webp"
+              alt="Picture of the author"
+              width="800"
+              height="450"
+              blurDataURL="/mintinfo.webp"
+              placeholder="/mintinfo.webp"
+            />
+            <Image
+              src="/lowell.png"
+              alt="Picture of the author"
+              width="509"
+              height="796"
+            />
+          </div>
+          <h2>Pillars of Fomo Sapiens:</h2>
           <p>
             Fomo Sapiens is a comprehensive NFT project that aims to stand out
             in the NFT space by incorporating various innovative concepts and
             features. The project is structured around four primary pillars:
           </p>
-          <h5>1. Fomos X Base: </h5>{" "}
+          <h3>1. Fomos X Base: </h3>{" "}
           <p>
             This pillar serves as the central hub of the Fomo Sapiens Ecosystem,
             overseeing all operational tasks and events while also directing the
@@ -60,7 +132,7 @@ const AboutProject = () => {
             tailored for use as social media profile pictures. Owners of these
             designs are granted intellectual property rights.
           </p>
-          <h5> 2. Fomos X Curated: </h5>
+          <h3> 2. Fomos X Curated: </h3>
           <p>
             {" "}
             Under this pillar, Fomo Sapiens showcases NFTs created by its
@@ -72,7 +144,7 @@ const AboutProject = () => {
             special art collections and custom merch products which will be
             produced by the help of streetwear fashion brand OLL.
           </p>
-          <h5> 3. Fomos X Gaming: </h5>
+          <h3> 3. Fomos X Gaming: </h3>
           <p>
             Fomo Sapiens introduces @theclashub , an award-winning multi-chain
             card game platform that hosts various NFT collections, including
@@ -82,7 +154,7 @@ const AboutProject = () => {
             token through an Initial DEX Offering (IDO), offering collectors a
             Play-to-Earn (P2E) opportunity.
           </p>
-          <h5> 4. Fomos X Platform: </h5>
+          <h3> 4. Fomos X Platform: </h3>
           <p>
             This pillar forms the foundation of Fomo Sapiens, encapsulating its
             platform and ecosystem, all geared towards delivering a seamless and
@@ -97,16 +169,33 @@ const AboutProject = () => {
             Sapiens offers a unique and immersive experience for NFT collectors,
             setting itself apart as a groundbreaking project in the NFT space.
           </p>
-          <Image
-            className="{mintInfo.banner}"
-            src="/mintinfo.png"
-            alt="Picture of the author"
-            width="560"
-            height="290"
-            blurDataURL="/mintinfo.webp"
-            placeholder="/mintinfo.webp"
-          />
-          <h5>Utilities: </h5>
+          <div className={aboutProject.first_image_container}>
+            <Image
+              src="/fomos2.png"
+              alt="Picture of the author"
+              width="1500"
+              height="1500"
+              blurDataURL="/mintinfo.webp"
+              placeholder="/mintinfo.webp"
+            />
+            <Image
+              src="/fomos4.png"
+              alt="Picture of the author"
+              width="1500"
+              height="1500"
+              blurDataURL="/mintinfo.webp"
+              placeholder="/mintinfo.webp"
+            />
+            <Image
+              src="/fomos1.png"
+              alt="Picture of the author"
+              width="1500"
+              height="1500"
+              blurDataURL="/mintinfo.webp"
+              placeholder="/mintinfo.webp"
+            />
+          </div>
+          <h2>Utilities: </h2>
           <p>
             {" "}
             Fomo Sapiens offers a range of utilities to enhance the experience
@@ -133,7 +222,7 @@ const AboutProject = () => {
             blend of art, gaming, community engagement, and unique opportunities
             in the NFT space.
           </p>
-          <h5> The Invitation App </h5>
+          <h2> The Invitation App </h2>
           <p>
             This utility is designed to cultivate a community prior to the
             launch by distributing allow list spots in a fair, organic, and
@@ -144,6 +233,26 @@ const AboutProject = () => {
               <Link href="/"> explore</Link>{" "}
             </u>
           </p>
+          <div className={aboutProject.first_image_container}>
+            <Image
+              className={aboutProject.full_image}
+              src="/invitation.png"
+              alt="Picture of the author"
+              width="1500"
+              height="1500"
+              blurDataURL="/mintinfo.webp"
+              placeholder="/mintinfo.webp"
+            />
+            <Image
+              className={aboutProject.full_image}
+              src="/story3.webp"
+              alt="Picture of the author"
+              width="1500"
+              height="1500"
+              blurDataURL="/mintinfo.webp"
+              placeholder="/mintinfo.webp"
+            />
+          </div>
         </div>
       </div>
     </div>
--- app/lore/page.js
+++ app/lore/page.js
@@ -1,7 +1,306 @@
 import lore from "../../styles/Lore.module.css";
+import Image from "next/image";
+import Link from "next/link";
 
 const Lore = () => {
-  return <div>LORE</div>;
+  return (
+    <div>
+      <div>
+        <br />
+        <Link href="/">BACK TO HOME PAGE</Link>
+        <h1>STORY & LORE OF FOMO SAPIENS</h1>
+        <div>
+          <p>
+            Until now, extraterrestrial ventures have
+            primarily served as sources of income for the wealthy and
+            governments, focusing on mining and tourism. The concept of a
+            sustainable and realistic "space civilization" has not been
+            seriously considered due to political reasons and the limitations of
+            the human body. However, science has progressed independently and
+            has now enabled us to overcome our most challenging problem.
+            Scientists have successfully evolved human cells to adapt to space
+            conditions, achieving a breakthrough in evolution through scientific
+            means rather than natural selection. This discovery sparked
+            significant FOMO (fear of missing out) among humanity, leading to a
+            surge in efforts related to extraterrestrial life. The pioneer of
+            synthetic evolution, scientists, established a decentralized company
+            and called for all related research to be gathered under its
+            umbrella. This company aims to establish a new space civilization,
+            and the people chosen to build this civilization outside Earth are
+            called "FOMO SAPIENS."
+          </p>
+          <br />
+          <h2>Why Pluto?</h2>
+          <p>
+            Pluto is the farthest planet from the sun, making it a logical base
+            for reaching planets outside the solar system. Additionally, it
+            features a heart-shaped plain on its surface.
+          </p>
+          <p>image</p>
+          <br />
+          <h2>How will they go?</h2>
+          <p>
+            A shuttle will be launched when Pluto is close enough to Earth,
+            utilizing the gravitational forces of the planets along the way to
+            aid in its journey. A crew will operate the shuttle for safety,
+            while thousands of passengers will sleep in pods. During this time,
+            the passengers will live in a virtual world via a "full dive VR
+            set." The crew can communicate with the passengers using the VR set,
+            and passengers can control humanoid robots to interact with the
+            physical world. Until they reach Pluto, passengers will continue
+            working and perhaps laying the foundations of the new civilization
+            in the virtual world. The spacecraft will also carry multipurpose
+            robots, drones, tools, equipment, materials, and genetically
+            modified seeds.
+          </p>
+          <h3>Biotech and Synthetic Evolution Elements</h3>
+          <p>
+            <b>Cell Evolver:</b>This technology evolves human cells to withstand
+            extreme temperatures, pressure, acidity levels, and radiation,
+            allowing cells to use various gases for energy instead of oxygen.
+            The energy generated also meets human energy needs and charges
+            biotech items. Evolved cells change color, with the skin turning
+            turquoise, becoming the most distinctive feature. Evolution cures
+            cellular diseases like cancer and greatly prevents new diseases.
+          </p>
+          <p>
+            <b>SB (Smart Blood): </b>Facilitates blood circulation in space by
+            following commands from SBC. It can carry any gas, and its speed,
+            density, and contents are adjustable by SBC.
+          </p>
+          <p>
+            <b>SVP (Smart Vascular Pads):</b>Works with SMP to facilitate SB
+            circulation and actively monitors blood-related health conditions,
+            reporting to SBC.
+          </p>
+          <p>
+            <b>SMP (Smart Muscular Pads): </b>Works with SVP to facilitate SB
+            circulation. In zero gravity, it simulates realistic muscle
+            movements to prevent muscle loss, even working muscles without
+            waking the users during sleep. It also enhances realistic movement
+            experiences in VR and RB.
+          </p>
+          <p>
+            <b>SBC (Second Brain Chip): </b>Controls other biotech products,
+            communicates with humans and computers, and stores information.
+          </p>
+          <p>
+            <b>RB (Robot Body):</b>Robot bodies controlled by human brains,
+            surpassing humans in size and strength, and enhanced with additional
+            technologies for various purposes.
+          </p>
+          <p>
+            <b>Full Dive VR Set:</b>Allows people to use their bodily signals in
+            a virtual world.
+          </p>
+
+          <p>image here</p>
+          <br />
+          <h5>NEW CIV PERKS</h5>
+          <p>
+            Meals will consist of lab-grown meat and genetically modified fruits
+            and vegetables with added multivitamins/minerals. Since these foods
+            have no fiber, there will be no need for excretion (they will get
+            used to this while still on Earth).
+            <br />
+            It has been detected that the sex cells produced after using the
+            evolver shit carry the new DNA codes. Therefore, the process is
+            considered more of a mutation than a modification, and it is
+            genetically passed on, meaning children born will also be suited for
+            living in space.
+            <br />
+            The foundations of buildings will use devices that apply centrifugal
+            force downward to increase or decrease weight. This will cause
+            buildings to float before an earthquake and stick to the ground
+            otherwise.
+            <br />
+            With a software uploaded to the SBC, individuals can train their
+            "personal artificial intelligences" that directly pull information
+            from their brains. These AIs can be used in different ways with
+            consent after individuals die and might sometimes cause trouble.
+            <br />
+            Deceased individuals will be sent to the core of the center in a
+            ceremony.
+            <br />
+            The access of general AIs used will be limited and primarily used
+            for automation.
+          </p>
+
+          <h2>Citizens will have tattoos indicating their roles/classes:</h2>
+          <ul>
+         
+
+            <li>
+              <h3>Warriors</h3>
+              <p>image</p>
+              <p>
+                The Warriors are a skilled and fearless faction of the Lowell
+                Foundation. Their mission is to explore other planets and defend
+                the new decentralized space civilization, using advanced
+                technology and combat training.{" "}
+              </p>
+              <span>
+                <p>image</p>
+                <p>image</p>
+              </span>
+            </li>
+
+            <li>
+              <h3>Scientists</h3>
+              <p>image</p>
+              <p>
+                The Scientists of the Lowell Foundation cover diverse fields
+                such as physics, chemistry, biology, astronomy, geology, and
+                environmental science. They will prioritize providing advanced
+                technology for the civilizations’ needs without political
+                constraints.{" "}
+              </p>
+              <span>
+                <p>image</p>
+                <p>image</p>
+              </span>
+            </li>
+
+            <li>
+              <h3>Philosophers</h3>
+              <p>image</p>
+              <p>
+                The Philosophers of the Lowell Foundation are a group of deep
+                thinkers who are dedicated to engaging with philosophical
+                questions. Their main mission is to cultivate wisdom and promote
+                critical thinking in future generations by taking on the
+                responsibility of education.
+              </p>
+              <span>
+                <p>image</p>
+                <p>image</p>
+              </span>
+            </li>
+
+            <li>
+              <h3>Artists</h3>
+              <p>image</p>
+              <p>
+                The Artists of Lowell Foundation are a group of creative
+                individuals dedicated to exploring the limitless potential of
+                artistic expression. From painting to sculpture, music to
+                literature, they embrace all forms of art and use their talents
+                to inspire and enrich the community
+              </p>
+              <span>
+                <p>image</p>
+                <p>image</p>
+              </span>
+            </li>
+
+            <li>
+              <h3>Diplomats</h3>
+              <p>image</p>
+              <p>
+                The Diplomats of the Lowell Foundation are responsible for
+                handling crises, running the foundation, planning space
+                explorations, managing resources, and establishing contact with
+                other civilizations. Their work requires close coordination with
+                the other factions.
+              </p>
+              <span>
+                <p>image</p>
+                <p>image</p>
+              </span>
+            </li>
+          </ul>
+          <div>
+            <h3>Chapters</h3>
+            <h5>Chapter 1: Selection and Training</h5>
+            <p>
+              The first mission of our story involves selecting and training a
+              group of individuals who are genetically compatible with space
+              travel. This group will be hand-picked for their unique skills and
+              expertise and will undergo rigorous training to prepare them for
+              the challenges of establishing a new civilization on Pluto.
+            </p>
+            <span>Lab Scene</span>
+            <p>
+              {" "}
+              “Trying to humanize the extraterrestrial environment was doomed to
+              fail. Instead, in order to live in space, the human body had to
+              evolve.” -Dr. Thiccfloyd{" "}
+            </p>
+            <span>image</span>
+            <br />
+            <span>Foundation & Lab Opening</span>
+            <p>
+              Dr. Thiccfloyd, who enabled the human body to adapt to space
+              conditions through synthetic evolution, founded the Lowell
+              Foundation to establish a decentralized space civilization
+              starting from Pluto and called for all research to be consolidated
+              within this organization.{" "}
+            </p>
+            <span>image</span>
+            <br />
+            <span>Invitation</span>
+            <p>
+              The Lowell Foundation volunteers' groundbreaking works sparked
+              FOMO in humanity and propelled studies on extraterrestrial life.
+              It was time to invite the pioneers - artists, warriors,
+              philosophers, diplomats, and scientists - who would later be known
+              as Fomo Sapiens.{" "}
+            </p>
+            <span>image</span>
+            <br />
+            <span>Tattoo Shop</span>
+            <p>
+              {" "}
+              Fomo Sapiens, chosen by Lowell Foundation to build a new
+              civilization on Pluto, get tattoos to show their fraction. These
+              markings signify their role and symbolize their dedication and
+              courage across the universe.
+            </p>
+            <span>image</span>
+            <br />
+            <span>Tattoo Shop</span>
+            <p>
+              Final preparations have been completed, and an exciting new era
+              for humanity in space is about to begin as the Fomo Sapiens board
+              the spacecraft known as Trans Neptunian Express, ready to embark
+              on their journey towards Pluto.{" "}
+            </p>
+            <span>image</span>
+            <h5>Chapter 2: Journey to Pluto*</h5>
+            The second mission involves embarking on the long journey to Pluto
+            aboard a state-of-the-art spacecraft. This journey will be filled
+            with challenges, including cramped quarters and the difficulties of
+            living and working together in a confined space for an extended
+            period of time.
+            <h5>Chapter 3: Establishing a Sustainable Society</h5>
+            <p>
+              Once the group arrives at Pluto, their next mission will be to
+              establish a sustainable way of life on the planet. This will
+              involve finding a way to generate food, water, and energy in the
+              hostile environment, as well as grappling with the challenges of
+              living in a decentralized society.
+            </p>
+            <h5>Chapter 4: Exploration and Expansion</h5>
+            <p>
+              As the group begins to establish their new society, their next
+              mission will be to explore the uncharted territories of Pluto and
+              to look for opportunities to expand their civilization. This will
+              involve mapping the planet, searching for valuable resources, and
+              establishing new settlements.
+            </p>
+            <h5>Chapter 5: Ensuring the Future of the New Civilization </h5>
+            <p>
+              The final mission of our story will involve ensuring the long-term
+              survival and success of the new civilization on Pluto. This will
+              involve continuing to adapt to the planet's harsh conditions,
+              developing new technologies and innovations, and working together
+              to overcome any challenges that may arise.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Lore;
