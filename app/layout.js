c06b09bad78f12875d8cf7ec202e18e607b75fb5
berkay-yuksel <98.berkay.yuksel@gmail.com>
2025-11-05 08:35:52 +0300

    fixed typo3

--- app/layout.js
+++ app/layout.js
@@ -10,7 +10,7 @@ export default function RootLayout({ children }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="color-scheme" content="light only" />
         <title>Wonder Boss NFT</title>
-        <meta property="og:title" content="My page title" key="title" />
+        <meta property="og:title" content="Wonder Boss" key="title" />
         <meta
           name="description"
           content="Wonder Boss is a large story-telling pfp NFT collection sitting on Solana network."
