dec2d91e193814b2b4b309d35c6c326642c036a5
berkay-yuksel <98.berkay.yuksel@gmail.com>
2026-06-02 23:23:57 +0300

    iphone share add

--- app/overlay-generator/page.js
+++ app/overlay-generator/page.js
@@ -34,6 +34,37 @@ const overlaysList = [
     light: "/overlay/card_light.png",
     dark: "/overlay/card_dark.png",
   },
+    {
+    id: "banana",
+    label: "Banana",
+    light: "/overlay/banana_light.png",
+    dark: "/overlay/banana_dark.png",
+  },
+    {
+    id: "cry",
+    label: "Cry",
+    light: "/overlay/cry_light.png",
+    dark: "/overlay/cry_dark.png",
+  },
+    {
+    id: "heart",
+    label: "Heart",
+    light: "/overlay/heart_light.png",
+    dark: "/overlay/heart_dark.png",
+  },
+    {
+    id: "midf",
+    label: "MidF",
+    light: "/overlay/midf_light.png",
+    dark: "/overlay/midf_dark.png",
+  },
+    {
+    id: "ok",
+    label: "OK",
+    light: "/overlay/ok_light.png",
+    dark: "/overlay/ok_dark.png",
+  },
+
 ];
 
 export default function OverlayGenerator() {
